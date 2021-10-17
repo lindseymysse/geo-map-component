f6729589fa8660c8342128f23223772db7cd0bfa
LNSY <lindsey.mysse@gmail.com>
2021-10-17 13:11:14 -0700

    deck gl, loading

--- geo-map.js
+++ geo-map.js
@@ -333,7 +333,25 @@ class GeoMap extends HTMLElement {
 
     this.setZoomClass()
 
+    const {MapboxLayer, ScatterplotLayer, ArcLayer} = deck;
+
+    
+
+    this.map.addLayer(new MapboxLayer({
+      id: 'deckgl-arc',
+      type: ArcLayer,
+      data: [
+        {source: [-122.3998664, 37.7883697], target: [-122.400068, 37.7900503]}
+      ],
+      getSourcePosition: d => d.source,
+      getTargetPosition: d => d.target,
+      getSourceColor: [255, 208, 0],
+      getTargetColor: [0, 128, 255],
+      getWidth: 8
+    }))
+
     this.dispatchEvent( new CustomEvent('MAP MOVE END', {detail: first_pos}))
+
 
 
     this.map.on('moveend', (e) => {
