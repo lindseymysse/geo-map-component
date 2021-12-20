a9bcce5664f811830bd3464028b9d9a61f6c94c7
LNSY <lindsey.mysse@gmail.com>
2021-12-20 04:59:46 -0800

    added real estate demo

---
+++ src/map-directions.js
@@ -0,0 +1,8 @@
+import GeoMapElement from './map-element.js'
+
+
+class MapDirections extends GeoMapElement {
+
+}
+
+customElements.define('map-directions', MapDirections)
---
+++ src/map-path.js
@@ -0,0 +1,8 @@
+import GeoMapElement from './map-element.js'
+
+
+class MapPath extends GeoMapElement {
+
+}
+
+customElements.define('map-path', MapPath)
