805ca35c13d3ea77f5558eda10ffac616439c8e8
LNSY <lindsey.asterius@gmail.com>
2022-12-28 11:44:33 -0800

    readded map-component js

--- geo-map-component.js
+++ geo-map-component.js
@@ -1,34 +1,89 @@
-
-
-
+import 'https://api.mapbox.com/mapbox-gl-js/v2.9.2/mapbox-gl.js';
+import 'https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-geocoder/v4.7.0/mapbox-gl-geocoder.min.js';
+import 'https://unpkg.com/three@0.126.0/build/three.min.js';
+import 'https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-geocoder/v5.0.0/mapbox-gl-geocoder.min.js';
+import { getURLValues, ready } from './helpers.js';
 
 class GeoMapComponent extends HTMLElement {
-  connectedCallback(){
-    this.attrs = this.getAttributeNames().reduce((acc, name) => {
-      return {...acc, [name]: this.getAttribute(name)};
-    }, {});
+  constructor() {
+    super();
+    if(typeof(mapboxgl) === 'undefined'){
+      return console.error('Geo Map component requires Mapbox to work');
+    }
 
+    const URLvalues = getURLValues();
 
-    const required_attributes = ['blah', 'blah-1', 'blah-2'];
+    this.access_token = this.getAttribute('accesstoken');
+    if(this.access_token === null){
+      return console.error('Geo Map component requires a MapBox access token');
+    }
+    this.removeAttribute('accesstoken');
+    mapboxgl.accessToken = this.access_token;
 
-    required_attributes.forEach(attribute => {
-      if(typeof this.attrs[attribute] === 'undefined'){
-        return console.error(`The attribute ${attribute} is required;`)
-      }
+    this.styleurl = this.getAttribute('styleurl');
+    if(this.styleurl === null || this.styleurl === ""){
+      console.warn('could not find style url, using the default');
+      this.styleurl = 'mapbox://styles/mapbox/streets-v11';
+    }
+    this.removeAttribute('styleurl');
+
+    this.latitude = this.getAttribute('latitude');
+    if(this.latitude === null) this.latitude = 0;
+    this.latitude = URLvalues.latitude ? URLvalues.latitude : this.latitude;
+
+    this.longitude = this.getAttribute('longitude');
+    if(this.longitude === null) this.longitude = 0;
+    this.longitude = URLvalues.longitude ? URLvalues.longitude : this.longitude;
+
+    this.zoom = this.getAttribute('zoom');
+    if(this.zoom === null) this.zoom = 1;
+    this.zoom = URLvalues.zoom ? URLvalues.zoom : this.zoom;
+
+    this.bearing = this.getAttribute('bearing');
+    if(this.bearing === null) this.bearing = 0;
+    this.bearing = URLvalues.bearing ? URLvalues.bearing : this.bearing;
+
+    this.pitch = this.getAttribute('pitch');
+    if(this.pitch === null) this.pitch = 0;
+    this.pitch = URLvalues.pitch ? URLvalues.pitch : this.pitch;
+
+    this.locked = this.getAttribute('locked');
+    if(this.locked === null){
+      this.locked = false;
+    } else {
+      this.locked = true;
+    };
+
+    this.slideshow = this.getAttribute('slideshow');
+
+  }
+
+  connectedCallback() {
+    ready(() => this.initialize())
+  }
+
+  async initialize(){
+    const el = document.createElement('map-container')
+    this.appendChild(el)
+    this.map = await new mapboxgl.Map({
+      container: el, // container ID
+      style: this.styleurl, // style URL
+      center: [this.longitude, this.latitude],
+      zoom: this.zoom,
+      bearing: this.bearing,
+      projection: 'globe',
+      pitch: this.pitch,
+      style: this.styleurl,
+      interactive: !this.locked
     })
 
+    this.initialized = true;
+    this.map.on('load', () => {this.mapLoaded()})
   }
 
-  static get observedAttributes() {
-    return [];
+  mapLoaded(){
+    
   }
-
-  attributeChangedCallback(name, old_value, new_value){
-    switch(name){
-      default:
-    }
-  }
-
 }
 
 customElements.define('geo-map', GeoMapComponent)
